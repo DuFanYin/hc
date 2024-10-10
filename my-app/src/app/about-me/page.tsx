5f9f371b655c9088ac55f1b654667700d248fdcd
Hang Zhengyang <hang.zhengyang1010@gmail.com>
2024-10-10 09:52:56 +0800

    123

---
+++ my-app/src/app/about-me/page.tsx
@@ -0,0 +1,37 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import Image from "next/image";
+import Naruto from "@/app/public/naruto2.jpg";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Terminal } from "lucide-react";
+
+export default function AboutMe() {
+  return (
+    <div className="p-5">
+        Hello world this is the about me route
+        <Card>
+            <CardHeader>
+                <CardTitle>
+                    Hello I am nicholas
+                </CardTitle>
+                <CardDescription>
+                    I like to code lots
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <Image src={Naruto} alt="1"/>
+                <div className="flex flex-row gap-2">
+                    <p className="font-bold">Name:</p>
+                    Nicholas
+                </div>
+                <div className="flex flex-row gap-2"><p className="font-bold">Major:</p>Information Systems</div>
+                <div className="flex flex-row gap-2"><p className="font-bold">Hobbies:</p>I like to code</div>
+            </CardContent>
+        </Card>
+        <Alert>
+            <Terminal className="h-4 w-4"/>
+            <AlertTitle>Heads up</AlertTitle>
+            <AlertDescription>I have added an alert</AlertDescription>
+        </Alert>
+    </div>
+  );
+}
---
+++ my-app/src/components/navbar/navigation-menu.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, navigationMenuTriggerStyle } from "../ui/navigation-menu";
+import { ModeToggle } from "../mode-toggle";
+import Image from "next/image";
+import Logo from "@/app/assets/logo.png"
+
+export function NavigationBar() {
+    return (
+        <NavigationMenu className="flex flex-row justify-between min-w-full list-none h-16 sticky top-0 p-5">
+            <div className="flex flex-row">
+            <Image src={Logo} height="40" width="40" alt="logo"/>
+            <NavigationMenuItem>
+                <Link href="/" legacyBehavior passHref>
+                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                        Home
+                    </NavigationMenuLink>
+                </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+                <Link href="/quiz" legacyBehavior passHref>
+                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                        Quiz
+                    </NavigationMenuLink>
+                </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+                <Link href="/about-me" legacyBehavior passHref>
+                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                        About me
+                    </NavigationMenuLink>
+                </Link>
+            </NavigationMenuItem>
+            </div>
+            <div>
+                <ModeToggle/>
+            </div>
+        </NavigationMenu>
+    )
+}
